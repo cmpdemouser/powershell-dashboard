80abfb22776d3a885922b0f674ce2206996c8203
cmpdemouser <cmpdemouser@gmail.com>
2026-01-15 01:09:58 +0530

    Update win32-msi-creator.ps1

--- scripts/win32-msi-creator.ps1
+++ scripts/win32-msi-creator.ps1
@@ -1,1 +1,9 @@
+<#
+.Title        Win32 MSI Creator
+.Category     Intune
+.Description  Create Win32 MSI apps automatically
+.Author       cmp demo user
+#>
+
+Write-Host "Win32 MSI Creator Script"
 
